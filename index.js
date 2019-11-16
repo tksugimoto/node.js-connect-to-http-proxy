909376cf4167ce0bdf0d44f93054af9a3df9c3fc
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-11-17 02:59:20 +0900

    refactor: プロキシサーバーへの CONNECT に net ではなく http を使用
    HTTPリクエストの生成・レスポンスのパース処理を自前でやるのはリスクになるため

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const net = require('net');
+const http = require('http');
 const assert = require('assert');
 const {
     parse: parseUrl,
@@ -19,23 +19,22 @@ assert(process.argv[4], 'destination-server port arg required.');
 const destHost = process.argv[3];
 const destPort = process.argv[4];
 
-const CRLF = '\r\n';
-
-const proxySocket = net.createConnection(proxyPort, proxyHost);
-proxySocket.once('connect', () => {
-    proxySocket.write(`CONNECT ${destHost}:${destPort} HTTP/1.0${CRLF}${CRLF}`);
-    proxySocket.once('data', data => {
-        const response = data.toString();
-        const statusLine = response.split(CRLF)[0];
-        const [/* version */, statusCode] = statusLine.split(' ');
-        if (statusCode !== '200') {
-            console.error(statusLine);
-            return;
-        }
-        process.stdin.pipe(proxySocket);
-        proxySocket.pipe(process.stdout);
-    });
+const proxyRequestOptions = {
+    hostname: proxyHost,
+    port: proxyPort,
+    method: 'CONNECT',
+    path: `${destHost}:${destPort}`,
+};
+const proxyRequest = http.request(proxyRequestOptions);
+proxyRequest.on('connect', (res, proxySocket) => {
+    if (res.statusCode !== 200) {
+        console.error(`${res.statusCode} ${res.statusMessage}`);
+        return;
+    }
+    process.stdin.pipe(proxySocket);
+    proxySocket.pipe(process.stdout);
 });
-proxySocket.on('error', err => {
+proxyRequest.on('error', err => {
     console.error(err);
 });
+proxyRequest.end();
